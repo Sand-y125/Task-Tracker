acf44177864eae77fb6c8e7eaee53c575388d00f
Sand-y125 <sandhyalamichhane098@gmail.com>
2026-07-30 11:43:57 +0545

    Fix backend paths and Render port

--- Backend/server.js
+++ Backend/server.js
@@ -6,19 +6,19 @@ connectDB();
 
 const app = express();
 
-const TaskRoutes = require('./src/Routes/TaskRoutes');
-const UserRoutes = require('./src/Routes/UserRoutes');
-const AuthRoutes = require('./src/Routes/AuthRoutes'); 
-const VerifyToken = require('./src/middleware/VerifyToken');
+const TaskRoutes = require('./src/routes/TaskRoutes');
+const UserRoutes = require('./src/routes/UserRoutes');
+const AuthRoutes = require('./src/routes/AuthRoutes');
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/task', TaskRoutes);
 app.use('/user', UserRoutes);
-app.use('/auth', AuthRoutes); 
+app.use('/auth', AuthRoutes);
 
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-    console.log('Express Server is running on port 5000');
+app.listen(PORT, () => {
+    console.log(`Express Server is running on port ${PORT}`);
 });
